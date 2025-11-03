481419f38f589ca4bbca8bd66fbd05e20be96d31
Loweezzt <lowe.vesterberg@elev.ga.ntig.se>
2025-11-03 11:14:17 +0100

    funkar hyfsat

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
-
-const PlayerName = "Lowe"
+const playerName = "Loweezzt"
 let playerHp = 100
 let enemyHp = 100
-Math.random()
-Math.floor(Math.random(1))
-Math.ceil(Math.random(6))
-
 
 function rollDice() {
     return Math.ceil(Math.random() * 6)
@@ -13,12 +8,31 @@ function rollDice() {
 
 console.log(rollDice())
 
-let playerRoll = rollDice()
-let enemyRoll = rollDice()
-    if (playerRoll > enemyRoll) {
-dmg = playerRoll - enemyRoll
-    } else if (enemyRoll > playerRoll) {
+const playerHpElement = document.querySelector("#player-hp")
+const enemyHpElement = document.querySelector("#enemy-hp")
 
-    } else {
-        console.log("Snyggt parerat.")
+function gameRound() {
+    if (playerHp <= 0) {
+        console.log("Du är död! Spelet är slut.")
+    } else if (enemyHp <= 0) {
+        console.log("Fienden är död! Du vinner!")
     }
+    let playerRoll = rollDice()
+    let enemyRoll = rollDice()
+    if (playerRoll > enemyRoll) {
+        const damage = playerRoll - enemyRoll
+        console.log(`Du slår fienden för ${damage}!`)
+        enemyHp -= damage
+    } else if (enemyRoll > playerRoll) {
+        const damage = enemyRoll - playerRoll
+        console.log(`Nedrans, du blir träffad för ${damage}!`)
+        playerHp -= damage
+    } else {
+        console.log("Snyggt parerat, inget händer!")
+    }
+    playerHpElement.textContent = playerHp
+    enemyHpElement.textContent = enemyHp
+}
+const playButton = document.querySelector("#play-button")
+playButton.addEventListener("click", gameRound)
+
